40c96540b03d7a2b5059b0db04b585d8ad7a5dce
Nick Zhao <nickzrz@hotmail.com>
2018-12-15 16:17:55 -0800

    FINSIHED!

--- Reverse_GuessingGame/Reverse_GuessingGame/Source.cpp
+++ Reverse_GuessingGame/Reverse_GuessingGame/Source.cpp
@@ -16,25 +16,48 @@ void toLower(std::string& data)
 
 int main()
 {
-	srand(time(NULL));
-
-	int randomNum = rand() % 100 + 1;
-	int prevRand = 0;
-	string answer = "";
-
 	while (true)
 	{
-		cout << "Please think of a number between 1 and 100. I will try to guess what it is. Is it " << randomNum << "?" << endl;
-		cin >> answer;
-		prevRand = randomNum;
-		toLower(answer);
-		if (answer == "too high")
+		srand(time(NULL));
+
+		int randomNum = rand() % 100 + 1;
+		string answer = "";
+		string answer2 = "";
+		int Max = 100;
+		int min = 0;
+		int count = 0;
+		cout << "Please think of a number between 1 and 100. I will try to guess what it is. ";
+		while (true)
 		{
-			randomNum = rand() % 1 + prevRand;
+			cout << "Is it " << randomNum << "?" << endl;
+			std::getline(cin, answer);
+
+
+			toLower(answer);
+			if (answer == "too high")
+			{
+				Max = randomNum;
+				randomNum = rand() % (Max - min) + min;
+				count++;
+			}
+			if (answer == "too low")
+			{
+				min = randomNum;
+				randomNum = rand() % (Max - min) + min;
+				count++;
+			}
+			if (answer == "correct")
+			{
+				cout << "Yay! It took me " << count << " tries to get it." << endl;
+				break;
+			}
+		}
+		cout << "Do you want to play again?" << endl;
+		cin >> answer2;
+		toLower(answer2);
+		if (answer2 == "no")
+		{
+			break;
 		}
 	}
-
-
-
-	system("PAUSE");
 }
